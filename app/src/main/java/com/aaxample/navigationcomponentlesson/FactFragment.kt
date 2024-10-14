6e45b7482d65e6f346a092fb0be91cafc2fdcbba
Sergey Boykov <sutemi67@gmail.com>
2024-10-14 19:33:43 +0300

    finished

--- app/src/main/java/com/aaxample/navigationcomponentlesson/FactFragment.kt
+++ app/src/main/java/com/aaxample/navigationcomponentlesson/FactFragment.kt
@@ -4,29 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.aaxample.navigationcomponentlesson.databinding.FragmentFactBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FactFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FactFragment : Fragment() {
     private lateinit var binding: FragmentFactBinding
     private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getString(ARGS_FACT)
         }
     }
 
@@ -40,28 +31,36 @@ class FactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.backButton.setOnClickListener {
+        val fact = requireArguments().getString(ARGS_FACT)
+        val image = if (fact.equals("Cat fact")) {
+            R.drawable.vat1
+        } else {
+            R.drawable.hams1
+        }
+        binding.factText.text = fact
 
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.nextButton.setOnClickListener {
+            findNavController().navigate(
+                R.id.imageFragment,
+                ImageFragment.createArgs(image)
+            )
         }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FactFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        const val ARGS_FACT = "fact"
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String) =
             FactFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARGS_FACT, param1)
                 }
             }
+
+        fun createArgs(fact: String): Bundle = bundleOf(ARGS_FACT to fact)
     }
 }
--- app/src/main/java/com/aaxample/navigationcomponentlesson/StartFragment.kt
+++ app/src/main/java/com/aaxample/navigationcomponentlesson/StartFragment.kt
@@ -4,19 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.aaxample.navigationcomponentlesson.databinding.FragmentStartBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StartFragment : Fragment() {
     private lateinit var binding: FragmentStartBinding
 
@@ -45,23 +40,22 @@ class StartFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.catPathButton.setOnClickListener {
-
+            findNavController().navigate(
+                R.id.action_startFragment_to_factFragment,
+                FactFragment.createArgs("Cat fact")
+            )
         }
         binding.hamsterPathButton.setOnClickListener {
-
+            findNavController().navigate(
+                R.id.action_startFragment_to_factFragment,
+                FactFragment.createArgs("Hamster fact")
+            )
         }
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             StartFragment().apply {
